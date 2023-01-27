549e8e1bba586e92df0affaf9b246bf1bae46df1
shahulhameed22 <113240623+shahulhameed22@users.noreply.github.com>
2023-01-27 13:16:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var connection  = require('./connection');
 var usersRouter = require('./task');
 var app = express();
 
-// view engine setup
+
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -26,7 +27,8 @@ app.use(session({
 app.use(flash());
 app.use('/task', usersRouter);
 
-// catch 404 and forward to error handler
+
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
